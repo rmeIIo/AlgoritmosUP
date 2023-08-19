e019c6c1be7e8524693a8e3c1aafb0b071ae7e0a
rmeIIo <j.rogercorreamello@gmail.com>
2023-08-18 21:03:30 -0300

    1808

--- 1208/ex11.c
+++ 1208/ex11.c
@@ -14,9 +14,9 @@ int main() {
 
     int eleitores = validos + brancos + nulos;
 
-    float percentValidos = ((float)validos / eleitores) * 100;
-    float percentBrancos = ((float)brancos / eleitores) * 100;
-    float percentNulos = ((float)nulos / eleitores) * 100;
+    float percentValidos = (validos / (float)eleitores) * 100.0;
+    float percentBrancos = (brancos / (float)eleitores) * 100.0;
+    float percentNulos = (nulos / (float)eleitores) * 100.0;
 
     printf("ELEIÇÕES APURADAS! Com um total de %i eleitores, o percentual de votos válidos foi de %.0f%%, %.0f%% brancos e %.0f%% nulos.\n", eleitores, percentValidos, percentBrancos, percentNulos);
 
